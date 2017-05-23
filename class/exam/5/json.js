22cf94599950126443f58e42dd17b8605bdb1100
Sq <574146616@qq.com>
2017-05-24 00:06:53 +0800

    利用JSON

---
+++ class/exam/5/json.js
@@ -0,0 +1,67 @@
+var productList =
+[
+  {
+    brand : "APPLE",
+    url : "img/iPhone7.jpg"
+  },
+  {
+    brand : "OPPO",
+    url : "img/OPPOR9s.jpg"
+  },
+  {
+    brand : "SAMSUNG",
+    url : "img/SAMSUNGS7.jpg"
+  },
+  {
+    brand : "SAMSUNG",
+    url : "img/SAMSUNGS8.jpg"
+  },
+  {
+    brand : "华为",
+    url : "img/华为P10.jpg"
+  },
+  {
+    brand : "华为",
+    url : "img/华为V9.jpg"
+  },
+  {
+    brand : "华为",
+    url : "img/华为畅享7.jpg"
+  },
+  {
+    brand : "小米",
+    url : "img/小米5s.jpg"
+  },
+  {
+    brand : "小米",
+    url : "img/小米6.jpg"
+  },
+  {
+    brand : "小米",
+    url : "img/小米MIX.jpg"
+  },
+  {
+    brand : "小米",
+    url : "img/小米Note2.jpg"
+  },
+  {
+    brand : "小米",
+    url : "img/红米Note4.jpg"
+  },
+  {
+    brand : "联想",
+    url : "img/联想乐檬K10.jpg"
+  },
+  {
+    brand : "酷派",
+    url : "img/酷派酷玩6.jpg"
+  },
+  {
+    brand : "魅族",
+    url : "img/魅族MX6.jpg"
+  },
+  {
+    brand : "魅族",
+    url : "img/魅族Pro6S.jpg"
+  },
+]
